00848d3bcb92a5af41e1596ba4e17259b277abbb
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-09 15:32:37 -0300

    feat(views): remove contato and sobre views

--- recipes/views.py
+++ recipes/views.py
@@ -2,12 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'nome': 'Pedro Tomáz'
-    })
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return render(request, 'sobre.html')
+    return render(request, 'recipes/pages/home.html')
